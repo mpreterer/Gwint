ec4c31a2bd5cf29b51b43677c2a89b70640c77c1
mpreter <dshabelyan@mail.ru>
2021-11-19 22:46:07 +0300

    Убрал проблему с display

--- js/continue.js
+++ js/continue.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
 	$('#continue').click(function () {
 		$('.container-player').hide(650);
+		$('.users').css('display','flex')
 		$('.users').fadeOut(650).show(650);
 	})
 });
