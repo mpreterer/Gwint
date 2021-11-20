b51a4e3019812240990d3511e6876a4874e7a1d0
mpreter <dshabelyan@mail.ru>
2021-11-20 20:51:04 +0300

    fix counter

--- js/counter.js
+++ js/counter.js
@@ -3,23 +3,27 @@ $( document ).ready(function() {
 	var n2 = 0;
 	
 	$('#inp1').val(n1);
-	$('#inp2').val(n1);
+	$('#inp2').val(n2);
 
 	$('.users__plus1').click(function () {
+		n1 = $('#inp1').val();
 		n1++;
 		$('#inp1').val(n1);
 	})
 	$('.users__minus1').click(function () {
+		n1 = $('#inp1').val();
 		n1--;
 		$('#inp1').val(n1);
 	})
 	////
 	$('.users__plus2').click(function () {
-		n1++;
-		$('#inp2').val(n1);
+		n2 = $('#inp2').val();
+		n2++;
+		$('#inp2').val(n2);
 	})
 	$('.users__minus2').click(function () {
-		n1--;
-		$('#inp2').val(n1);
+		n2 = $('#inp2').val();
+		n2--;
+		$('#inp2').val(n2);
 	})
 });
