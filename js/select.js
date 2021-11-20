8d88ce7924d0b360074d5daec2476a67a98566a1
mpreter <dshabelyan@mail.ru>
2021-11-20 20:42:59 +0300

    new version

--- js/select.js
+++ js/select.js
@@ -1,5 +1,7 @@
 $( document ).ready(function() {
 	$('#continue').attr('disabled','disabled');
+	$('#color1').val('none');
+	$('#color2').val('none');
 
 	$('#color1').on('change', function () {
 		if ($('#color1').val() !== 'none' && $('#color2').val() !== 'none') {
@@ -8,26 +10,38 @@ $( document ).ready(function() {
 			$('#continue').attr('disabled','disabled');
 		}
 
+		if ($('#color1').val() !== 'none') {
+			$('.slt-player__player1').css('color','#fff');
+		} else {
+			$('.slt-player__player1').css('color','#000');
+		}
+
+		if ($('#color2').val() !== 'none') {
+			$('.slt-player__player2').css('color','#fff');
+		} else {
+			$('.slt-player__player2').css('color','#000');
+		}
+
 		if ($('#color1').val()=='blue') {
-			$('.slt-player__player1').css('background-color','#0186E5');
-			$('.users__bg1').css('background-color','#0186E5');
+			$('.slt-player__player1').css('background-image','url(images/sever.jpg)');
+			$('.users__bg1').css('background-image','url(images/sever.jpg)');
 		}
 
 		else if ($('#color1').val()=='red') {
-			$('.slt-player__player1').css('background-color','#A90000');
-			$('.users__bg1').css('background-color','#A90000');
+			$('.slt-player__player1').css('background-image','url(images/chudo2.jpg)');
+			$('.users__bg1').css('background-image','url(images/chudo2.jpg)');
 		}
 
 		else if ($('#color1').val()=='green') {
-			$('.slt-player__player1').css('background-color','#287824');
-			$('.users__bg1').css('background-color','#287824');
+			$('.slt-player__player1').css('background-image','url(images/elphi.jpg)');
+			$('.users__bg1').css('background-image','url(images/elphi.jpg)');
 		}
 
 		else if ($('#color1').val()=='gold') {
-			$('.slt-player__player1').css('background-color','#C4A349');
-			$('.users__bg1').css('background-color','#C4A349');
+			$('.slt-player__player1').css('background-image','url(images/nilf.jpg)');
+			$('.users__bg1').css('background-image','url(images/nilf.jpg)');
 		} else {
-			$('.slt-player__player1').css('background-color','#fff');
+			$('.slt-player__player1').css('background-image','url(images/)');
 		}
 	})
 
@@ -39,23 +53,23 @@ $( document ).ready(function() {
 		}
 
 		if ($('#color2').val()=='blue') {
-			$('.slt-player__player2').css('background-color','#0186E5');
-			$('.users__bg2').css('background-color','#0186E5');
+			$('.slt-player__player2').css('background-image','url(images/sever.jpg)');
+			$('.users__bg2').css('background-image','url(images/sever.jpg)');
 		}
 
 		else if ($('#color2').val()=='red') {
-			$('.slt-player__player2').css('background-color','#A90000');
-			$('.users__bg2').css('background-color','#A90000');
+			$('.slt-player__player2').css('background-image','url(images/chudo2.jpg)');
+			$('.users__bg2').css('background-image','url(images/chudo2.jpg)');
 		}
 
 		else if ($('#color2').val()=='green') {
-			$('.slt-player__player2').css('background-color','#287824');
-			$('.users__bg2').css('background-color','#287824');
+			$('.slt-player__player2').css('background-image','url(images/elphi.jpg)');
+			$('.users__bg2').css('background-image','url(images/elphi.jpg)');
 		}
 
 		else if ($('#color2').val()=='gold') {
-			$('.slt-player__player2').css('background-color','#C4A349');
-			$('.users__bg2').css('background-color','#C4A349');
+			$('.slt-player__player2').css('background-image','url(images/nilf.jpg)');
+			$('.users__bg2').css('background-image','url(images/nilf.jpg)');
 		} else {
 			$('.slt-player__player2').css('background-color','#fff');
 		}
